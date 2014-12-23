7413ac0591ca091e08890ace6fb03c591e258349
Sam Spilsbury <smspillaz@gmail.com>
2014-12-23 15:35:33 +0800

    Fix numerous problems which arose when testing on a python project.
     1. Multi-variable arguments were not being concatenated correctly
     2. Check exit codes and return as appropriate
     3. Suppress spurious output
     4. Configure python-lint for maximum warnings

--- travis/shell-lint.sh
+++ travis/shell-lint.sh
@@ -10,14 +10,11 @@ echo "   ... Installing requirements"
 cabal install shellcheck > /dev/null 2>&1
 pip install bashlint > /dev/null 2>&1
 
-# Always exclude .git
-exclusions="$(pwd)/.git"
-
 while getopts "d:x:" opt; do
     case "$opt" in
-    x) exclusions+=$OPTARG
+    x) exclusions+=" $OPTARG"
        ;;
-    d) directories+=$OPTARG
+    d) directories+=" $OPTARG"
        ;;
     esac
 done
@@ -39,16 +36,25 @@ function get_exclusions_arguments() {
     eval "${result}"="'${cmd_append}'"
 }
 
-get_exclusions_arguments excl_args
+failures=0
+
+function check_status_of() {
+    output_file=$(mktemp)
+    eval "$@" > "${output_file}" 2>&1
+    if [[ $? != 0 ]] ; then
+        failures=$((failures + 1))
+        cat "${output_file}"
+    fi
+}
 
 echo "   ... Linting files"
-
+get_exclusions_arguments excl_args
 for directory in ${directories} ; do
     cmd="find ${directory} -type f -name \"*.sh\" ${excl_args}"
     shell_files=$(eval "${cmd}")
 
     for file in ${shell_files} ; do
-        shellcheck "${file}"
-        bashlint "${file}"
+        check_status_of shellcheck "${file}"
+        check_status_of bashlint "${file}"
     done
 done
