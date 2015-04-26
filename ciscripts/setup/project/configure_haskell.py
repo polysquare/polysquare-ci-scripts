4853ca60d96ee796a5885e348d9db37e89191b3f
Sam Spilsbury <smspillaz@gmail.com>
2015-04-26 02:41:54 -0700

    Don't double-activate container.
    It is already activated correctly when the installation runs.

--- ciscripts/setup/project/configure_haskell.py
+++ ciscripts/setup/project/configure_haskell.py
@@ -124,12 +124,12 @@ def get(container, util, shell, version, installer=_no_installer_available):
                         with open(installed_stamp, "w") as stamp:
                             stamp.write("done")
 
-                    with self.activated(util):
-                        util.execute(container,
-                                     util.long_running_suppressed_output(),
-                                     "cabal",
-                                     "install",
-                                     pkg_name)
+                    # Assumes that container is already active
+                    util.execute(container,
+                                 util.long_running_suppressed_output(),
+                                 "cabal",
+                                 "install",
+                                 pkg_name)
 
         def clean(self, util):
             """Clean out cruft in the container."""
