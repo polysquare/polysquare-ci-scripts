33ebdf3434582bdd918d817e3d72cda6546387f5
Sam Spilsbury <smspillaz@gmail.com>
2015-05-10 10:20:23 +0800

    Ensure that files are closed before reading them.
    On Windows, a PermissionError will be raised if a process attempts
    to a read a file before another process has finished writing it.
    
    The same also goes for deleting a file which is open by another or
    the same process.

--- test/test_project.py
+++ test/test_project.py
@@ -287,45 +287,40 @@ class TestProjectContainerSetup(TestCase):
 
     def test_lint_with_style_guide_linter_success(self):
         """Success code if all files satisfy style guide linter."""
-        with tempfile.NamedTemporaryFile(mode="wt",
-                                         dir=os.getcwd(),
-                                         suffix=".sh") as f:
-            write_valid_header(f)
+        with open(os.path.join(os.getcwd(), "success.sh"),
+                  "wt") as success_file:
+            write_valid_header(success_file)
 
-            self.assertThat("check/project/lint.py",
-                            CIScriptExitsWith(0,
-                                              self.__class__.container,
-                                              self.__class__.util,
-                                              extensions=["sh"]))
+        self.assertThat("check/project/lint.py",
+                        CIScriptExitsWith(0,
+                                          self.__class__.container,
+                                          self.__class__.util,
+                                          extensions=["sh"]))
 
     def test_lint_with_style_guide_linter_failure(self):
         """Failure code if one file doesn't satisfy style guide linter."""
-        with tempfile.NamedTemporaryFile(mode="wt",
-                                         dir=os.getcwd(),
-                                         suffix=".sh") as f:
-            write_invalid_header(f)
-            container = self.__class__.container
+        with open(os.path.join(os.getcwd(), "failure.sh"),
+                  "wt") as failure_file:
+            write_invalid_header(failure_file)
 
-            self.assertThat("check/project/lint.py",
-                            CIScriptExitsWith(1,
-                                              container,
-                                              self.__class__.util,
-                                              extensions=["sh"]))
+        self.assertThat("check/project/lint.py",
+                        CIScriptExitsWith(1,
+                                          self.__class__.container,
+                                          self.__class__.util,
+                                          extensions=["sh"]))
 
     def test_lint_files_in_multiple_subdirectories(self):
         """Style guide linter runs over multiple subdirectories."""
         success_dir = tempfile.mkdtemp(dir=os.getcwd())
-        success_file = tempfile.NamedTemporaryFile(mode="wt",
-                                                   dir=success_dir,
-                                                   suffix=".sh")
-
         failure_dir = tempfile.mkdtemp(dir=os.getcwd())
-        failure_file = tempfile.NamedTemporaryFile(mode="wt",
-                                                   dir=failure_dir,
-                                                   suffix=".sh")
 
-        write_valid_header(success_file)
-        write_invalid_header(failure_file)
+        with open(os.path.join(success_dir, "success.sh"),
+                  "wt") as success_file:
+            write_valid_header(success_file)
+
+        with open(os.path.join(failure_dir, "failure.sh"),
+                  "wt") as failure_file:
+            write_invalid_header(failure_file)
 
         self.assertThat("check/project/lint.py",
                         CIScriptExitsWith(1,
@@ -337,16 +332,13 @@ class TestProjectContainerSetup(TestCase):
 
     def test_lint_files_with_multiple_extensions(self):
         """Style guide linter runs over multiple extensions."""
-        cwd = os.getcwd()
-        success_file = tempfile.NamedTemporaryFile(mode="wt",
-                                                   dir=cwd,
-                                                   suffix=".zh")
-        failure_file = tempfile.NamedTemporaryFile(mode="wt",
-                                                   dir=cwd,
-                                                   suffix=".sh")
+        with open(os.path.join(os.getcwd(), "success.zh"),
+                  "wt") as success_file:
+            write_valid_header(success_file)
 
-        write_valid_header(success_file)
-        write_invalid_header(failure_file)
+        with open(os.path.join(os.getcwd(), "failure.sh"),
+                  "wt") as failure_file:
+            write_invalid_header(failure_file)
 
         self.assertThat("check/project/lint.py",
                         CIScriptExitsWith(1,
@@ -356,16 +348,13 @@ class TestProjectContainerSetup(TestCase):
 
     def test_files_can_be_excluded_from_linting(self):
         """Exclude certain files from style guide linter."""
-        cwd = os.getcwd()
-        success_file = tempfile.NamedTemporaryFile(mode="wt",
-                                                   dir=cwd,
-                                                   suffix=".sh")
-        failure_file = tempfile.NamedTemporaryFile(mode="wt",
-                                                   dir=cwd,
-                                                   suffix=".zh")
+        with open(os.path.join(os.getcwd(), "success.zh"),
+                  "wt") as success_file:
+            write_valid_header(success_file)
 
-        write_valid_header(success_file)
-        write_invalid_header(failure_file)
+        with open(os.path.join(os.getcwd(), "failure.sh"),
+                  "wt") as failure_file:
+            write_invalid_header(failure_file)
 
         fail_path = os.path.realpath(failure_file.name)
 
@@ -378,20 +367,17 @@ class TestProjectContainerSetup(TestCase):
 
     def test_many_files_can_be_excluded_from_linting(self):
         """Exclude many files from style guide linting."""
-        cwd = os.getcwd()
-        success_file = tempfile.NamedTemporaryFile(mode="wt",
-                                                   dir=cwd,
-                                                   suffix=".sh")
-        failure_file = tempfile.NamedTemporaryFile(mode="wt",
-                                                   dir=cwd,
-                                                   suffix=".zh")
-        second_failure_file = tempfile.NamedTemporaryFile(mode="wt",
-                                                          dir=cwd,
-                                                          suffix=".zh")
+        with open(os.path.join(os.getcwd(), "success.sh"),
+                  "wt") as success_file:
+            write_valid_header(success_file)
 
-        write_valid_header(success_file)
-        write_invalid_header(failure_file)
-        write_invalid_header(second_failure_file)
+        with open(os.path.join(os.getcwd(), "failure.zh"),
+                  "wt") as failure_file:
+            write_invalid_header(failure_file)
+
+        with open(os.path.join(os.getcwd(), "2failure.zh"),
+                  "wt") as second_failure_file:
+            write_invalid_header(second_failure_file)
 
         fail_path = os.path.realpath(failure_file.name)
         second_fail_path = os.path.realpath(second_failure_file.name)
@@ -408,7 +394,7 @@ class TestProjectContainerSetup(TestCase):
 
     def test_linting_of_markdown_documentation_with_success(self):
         """Lint markdown documentation with success exit code."""
-        with tempfile.NamedTemporaryFile(dir=os.getcwd(), suffix=".md"):
+        with open(os.path.join(os.getcwd(), "documentation.md"), "wt"):
             self.assertThat("check/project/lint.py",
                             CIScriptExitsWith(0,
                                               self.__class__.container,
@@ -417,14 +403,13 @@ class TestProjectContainerSetup(TestCase):
 
     def test_linting_of_markdown_documentation_with_failure(self):
         """Lint markdown documentation with success exit code."""
-        with tempfile.NamedTemporaryFile(mode="wt",
-                                         dir=os.getcwd(),
-                                         suffix=".md") as f:
-            f.write("Level One\n==\n\n## Level Two ##\n")
-            f.flush()
+        with open(os.path.join(os.getcwd(), "documentation.md"),
+                  "wt") as markdown_file:
+            markdown_file.write("Level One\n==\n\n## Level Two ##\n")
+            markdown_file.flush()
 
-            self.assertThat("check/project/lint.py",
-                            CIScriptExitsWith(1,
-                                              self.__class__.container,
-                                              self.__class__.util,
-                                              extensions=["other"]))
+        self.assertThat("check/project/lint.py",
+                        CIScriptExitsWith(1,
+                                          self.__class__.container,
+                                          self.__class__.util,
+                                          extensions=["other"]))
