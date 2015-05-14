7900f83b0b949e3db2c7d3adc4285a121848b374
Sam Spilsbury <smspillaz@gmail.com>
2015-05-14 11:31:28 +0800

    Don't install ruby unless we need it

--- ciscripts/setup/project/setup.py
+++ ciscripts/setup/project/setup.py
@@ -23,24 +23,26 @@ def run(cont, util, shell, argv=None):
                             help="""Do not install markdownlint""",
                             action="store_true")
         parse_result = parser.parse_args(argv or list())
-        rb_ver = defaultdict(lambda: "2.1.5",
-                             Windows="2.1.6")
+
+        config_python = "setup/project/configure_python.py"
         py_ver = defaultdict(lambda: "2.7.9")
 
         py_util = cont.fetch_and_import("python_util.py")
-        config_python = "setup/project/configure_python.py"
-        config_ruby = "setup/project/configure_ruby.py"
-
-        rb_cont = cont.fetch_and_import(config_ruby).run(cont,
-                                                         util,
-                                                         shell,
-                                                         rb_ver)
         py_cont = cont.fetch_and_import(config_python).run(cont,
                                                            util,
                                                            shell,
                                                            py_ver)
 
         if not parse_result.no_mdl:
+            config_ruby = "setup/project/configure_ruby.py"
+            rb_ver = defaultdict(lambda: "2.1.5",
+                                 Windows="2.1.6")
+
+            rb_cont = cont.fetch_and_import(config_ruby).run(cont,
+                                                             util,
+                                                             shell,
+                                                             rb_ver)
+
             with util.Task("""Installing markdownlint"""):
                 util.where_unavailable("mdl",
                                        util.execute,
--- container-setup.py
+++ container-setup.py
@@ -14,16 +14,9 @@ from collections import defaultdict
 def run(cont, util, shell, argv=None):
     """Set up language runtimes and pass control to python project script."""
     with util.Task("""Installing all necessary language runtimes"""):
-        rb_ver = defaultdict(lambda: "2.1.5", Windows="2.1.6")
-        py_ver = defaultdict(lambda: "2.7.9")
         hs_ver = defaultdict(lambda: "7.8.4")
-
-        configure_ruby = "setup/project/configure_ruby.py"
-        configure_python = "setup/project/configure_python.py"
         configure_haskell = "setup/project/configure_haskell.py"
 
-        cont.fetch_and_import(configure_ruby).run(cont, util, shell, rb_ver)
-        cont.fetch_and_import(configure_python).run(cont, util, shell, py_ver)
         cont.fetch_and_import(configure_haskell).run(cont, util, shell, hs_ver)
 
     cont.fetch_and_import("setup/python/setup.py").run(cont, util, shell, argv)
