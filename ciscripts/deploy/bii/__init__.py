f2ea8932a1836b5c93f632d636d624391467e7c0
Sam Spilsbury <smspillaz@gmail.com>
2015-08-03 21:31:16 +0800

    Add deploy/bii/deploy.py
    This script simply moves the bii project back into place, such
    that bii publish can be run

---
+++ ciscripts/deploy/bii/__init__.py
@@ -0,0 +1,6 @@
+# /ciscripts/deploy/bii/__init__.py
+#
+# Module loader file for /ciscripts/deploy/bii.
+#
+# See /LICENCE.md for Copyright information
+"""Module loader file for /ciscripts/deploy/bii."""
