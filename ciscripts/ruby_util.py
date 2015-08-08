6da55cc4d0604b2226ba6f82cd8e7a09be8c701e
Sam Spilsbury <smspillaz@gmail.com>
2015-08-09 02:21:05 +0800

    Discover any system installed rubies and prefer them
    Set GEM_HOME and PATH such that we can install gems and
    binaries inside the container, but otherwise use the
    system ruby installation from the outside.
    
    This will save some space on container caches.

--- ciscripts/ruby_util.py
+++ ciscripts/ruby_util.py
@@ -5,8 +5,60 @@
 # See /LICENCE.md for Copyright information
 """Ruby related utility functions."""
 
+import fnmatch
 
-def gem_install(container, util, *args, **kwargs):
+import os
+
+import re
+
+import subprocess
+
+
+_KNOWN_RUBY_INSTALLATIONS = dict()
+
+
+# suppress(invalid-name)
+def get_ruby_version_from_specified(ruby_executable, precision):
+    """Get python version at precision from specified ruby_executable."""
+    output = subprocess.Popen([ruby_executable, "--version"],
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE).communicate()
+    version = "".join([o.decode() for o in output])
+    version = ".".join(version.split(" ")[1].split(".")[0:precision]).strip()
+    return re.compile(r"([0-9\.]+)").match(version).group(1)
+
+
+def discover_rubies():
+    """Search PATH for ruby installations and return as dictionary.
+
+    Each key is a ruby version and the value corresponds to the location
+    of that ruby installation on disk.
+    """
+    if len(_KNOWN_RUBY_INSTALLATIONS.keys()):
+        return _KNOWN_RUBY_INSTALLATIONS
+
+    for path_component in os.environ.get("PATH", "").split(os.pathsep):
+        try:
+            dir_contents = os.listdir(path_component)
+        except OSError:
+            continue
+
+        candidates = set()
+        candidates |= set(fnmatch.filter(dir_contents, "ruby"))
+        candidates |= set(fnmatch.filter(dir_contents, "ruby.exe"))
+
+        # Make everything absolute again, remove symlinks
+        candidates = set([os.path.join(path_component, c) for c in candidates])
+        candidates = set([p for p in candidates if not os.path.islink(p)])
+
+        _KNOWN_RUBY_INSTALLATIONS.update({
+            get_ruby_version_from_specified(p, 3): p for p in candidates
+        })
+
+    return _KNOWN_RUBY_INSTALLATIONS
+
+
+def gem_install(container, rb_container, util, *args, **kwargs):
     """Install package using gem, specified in args.
 
     We automatically add the --conservative --no-ri --no-rdoc options
@@ -19,5 +71,7 @@ def gem_install(container, util, *args, **kwargs):
                         "--conservative",
                         "--no-ri",
                         "--no-rdoc",
+                        "--bindir",
+                        rb_container.gem_binary_directory(),
                         *args,
                         **kwargs)
--- ciscripts/setup/project/configure_ruby.py
+++ ciscripts/setup/project/configure_ruby.py
@@ -24,13 +24,18 @@ def get(container, util, shell, ver_info):
     del util
 
     version = ver_info[platform.system()]
-
-    if os.environ.get("POLYSQUARE_PREINSTALLED_RUBY", None):
-        container_path = os.environ["POLYSQUARE_PREINSTALLED_RUBY"]
-    else:
-        container_path = os.path.join(container.language_dir("ruby"),
-                                      "versions",
-                                      version)
+    container_path = os.path.join(container.language_dir("ruby"),
+                                  "versions",
+                                  version)
+    try:
+        with open(os.path.join(container_path,
+                               "system-installation"), "r") as install_info:
+            system_installation = install_info.read().strip()
+        with open(os.path.join(container_path, "version"),
+                  "r") as installed_version_info:
+            version = installed_version_info.read().strip()
+    except IOError:
+        system_installation = container_path
 
     # This class is intended to be used through LanguageBase, so
     # most of its methods are private
@@ -43,7 +48,7 @@ def get(container, util, shell, ver_info):
         # pylint can't detect that this is a new-style class
         #
         # suppress(super-on-old-class)
-        def __init__(self, version, installation, shell):
+        def __init__(self, version, installation, system_installation, shell):
             """Initialize a ruby installation for this version."""
             super(RubyContainer, self).__init__(installation,
                                                 "ruby",
@@ -52,7 +57,9 @@ def get(container, util, shell, ver_info):
 
             self._version = version
             self._installation = installation
+            self._system_installation = system_installation
             assert os.path.exists(self._installation)
+            assert os.path.exists(self._system_installation)
 
             if (platform.system() == "Darwin" and
                     not os.path.exists("/etc/openssl/cert.pem")):
@@ -81,27 +88,32 @@ def get(container, util, shell, ver_info):
                                     matching=["*unins000.dat"])
 
         @staticmethod
-        def _get_gem_dirs(installation, version):
+        def _get_gem_dirs(user_installation, system_installation, version):
             """Given an installation, return a GemDirs set."""
             minor_ver = ".".join(version.split("-")[0].split(".")[:2]) + ".0"
 
-            system_ruby_path = os.path.join(installation,
+            system_ruby_path = os.path.join(system_installation,
                                             "lib",
                                             "ruby",
                                             minor_ver)
             assert os.path.exists(system_ruby_path)
 
             return GemDirs(system=system_ruby_path,
-                           site=os.path.join(installation,
+                           site=os.path.join(system_installation,
                                              "lib",
                                              "ruby",
                                              "site_ruby",
                                              minor_ver),
-                           home=os.path.join(installation))
+                           home=os.path.join(user_installation))
+
+        def gem_binary_directory(self):
+            """Where binaries installed by gem install should be located."""
+            return os.path.join(self._installation, "bin")
 
         def _active_environment(self, tuple_type):
             """Return variables that make up this container's active state."""
             gem_dirs = RubyContainer._get_gem_dirs(self._installation,
+                                                   self._system_installation,
                                                    self._version)
             env_to_overwrite = {
                 "GEM_PATH": "{0}{s}{1}{s}{2}".format(gem_dirs.home,
@@ -111,13 +123,17 @@ def get(container, util, shell, ver_info):
                 "GEM_HOME": gem_dirs.home
             }
             env_to_prepend = {
-                "PATH": os.path.join(self._installation, "bin")
+                "PATH": os.pathsep.join([
+                    os.path.join(self._system_installation, "bin"),
+                    os.path.join(self._installation, "bin")
+                ])
+
             }
 
             return tuple_type(overwrite=env_to_overwrite,
                               prepend=env_to_prepend)
 
-    return RubyContainer(version, container_path, shell)
+    return RubyContainer(version, container_path, system_installation, shell)
 
 
 def posix_ruby_installer(lang_dir, ruby_build_dir, container, util, shell):
@@ -192,17 +208,64 @@ def windows_ruby_installer(lang_dir, ruby_build_dir, container, util, shell):
     return install
 
 
-def pre_existing_ruby(lang_dir, ruby_build_dir, container, util, shell):
-    """Use pre-installed ruby."""
-    del ruby_build_dir
-    del lang_dir
+def _write_system_installation_info(container,
+                                    installation_dir,
+                                    ruby_executable):
+    """Write system installation information in lang_dir.
 
+    This puts some information into installation_dir about the system version
+    of ruby that we are using, including its actual version number
+    and its install path.
+    """
+    assert not os.path.exists(installation_dir)
+    try:
+        os.makedirs(installation_dir)
+    except OSError as error:
+        if error.errno != errno.EEXIST:
+            raise error
+
+    ruby_path_components = os.path.split(os.path.dirname(ruby_executable))[:-1]
+
+    with open(os.path.join(installation_dir, "system-installation"),
+              "w") as install_info:
+        install_info.write(os.path.join(*(ruby_path_components)))
+
+    with open(os.path.join(installation_dir, "version"),
+              "w") as installed_version_info:
+        rb_util = container.fetch_and_import("ruby_util.py")
+        inst_ver = rb_util.get_ruby_version_from_specified(ruby_executable,
+                                                           3)
+        installed_version_info.write(inst_ver)
+
+
+def pre_existing_ruby(lang_dir, ruby_executable, container, util, shell):
+    """Use pre-installed ruby."""
     def install(version):
         """Use system installation directory."""
-        with util.Task("Using system installation"):
+        installation_dir = os.path.join(lang_dir, "versions", version)
+        if not os.path.exists(installation_dir):
+            _write_system_installation_info(container,
+                                            installation_dir,
+                                            ruby_executable)
+
+        with util.Task("""Using system installation"""):
             return get(container, util, shell, defaultdict(lambda: version))
 
     return install
+
+
+def _usable_preinstalled_ruby(container, version):
+    """Return any pre-installed ruby matching version that we can use."""
+    py_util = container.fetch_and_import("ruby_util.py")
+    preinstalled_rubies = py_util.discover_rubies()
+    requested_components = version.count(".") + 1
+
+    for candidate_version, candidate_path in preinstalled_rubies.items():
+        cand = ".".join(candidate_version.split(".")[:requested_components])
+        if cand == version:
+            return candidate_path
+
+    return None
 
 
 def run(container, util, shell, ver_info):
@@ -218,11 +281,12 @@ def run(container, util, shell, ver_info):
 
     lang_dir = container.language_dir("ruby")
     ruby_build_dir = os.path.join(lang_dir, "build")
+    usable = _usable_preinstalled_ruby(container, version)
 
     with util.Task("""Configuring ruby"""):
-        if (os.environ.get("POLYSQUARE_PREINSTALLED_RUBY", None) and
-                os.path.exists(os.environ["POLYSQUARE_PREINSTALLED_RUBY"])):
+        if usable:
             ruby_installer = pre_existing_ruby
+            ruby_build_dir = usable
         elif platform.system() in ("Linux", "Darwin"):
             ruby_installer = posix_ruby_installer
         elif platform.system() == "Windows":
