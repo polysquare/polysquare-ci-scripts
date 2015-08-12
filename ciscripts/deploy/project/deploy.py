b2f3d83edb4b3408190fe72ee30f5d552b1a5698
Sam Spilsbury <smspillaz@gmail.com>
2015-08-12 23:27:40 +0800

    project: Run travis-bump-version in python container

--- ciscripts/deploy/project/deploy.py
+++ ciscripts/deploy/project/deploy.py
@@ -15,10 +15,19 @@ import argparse
 import os
 
 
+def _get_python_container(cont, util, shell):
+    """Get python container to install linters into."""
+    config_python = "setup/project/configure_python.py"
+    py_ver = util.language_version("python3")
+
+    return cont.fetch_and_import(config_python).get(cont,
+                                                    util,
+                                                    shell,
+                                                    py_ver)
+
+
 def run(cont, util, shell, argv=None):
     """Invoke travis-bump-version if possible."""
-    del shell
-
     parser = argparse.ArgumentParser(description="""Automatically bump """
                                                  """version on successful """
                                                  """Travis-CI builds.""")
@@ -32,12 +41,13 @@ def run(cont, util, shell, argv=None):
 
     if api_key and job_slug and result.bump_version_on:
         with util.Task("""Pushing version bump to """ + job_slug):
-            util.execute(cont,
-                         util.running_output,
-                         "travis-bump-version",
-                         *(result.bump_version_on +
-                           ["--api-token",
-                            api_key,
-                            "--repo",
-                            job_slug]),
-                         allow_failure=True)
+            with _get_python_container(cont, util, shell):
+                util.execute(cont,
+                             util.running_output,
+                             "travis-bump-version",
+                             *(result.bump_version_on +
+                               ["--api-token",
+                                api_key,
+                                "--repo",
+                                job_slug]),
+                             allow_failure=True)
