880b449fcfb029047fdff40a314a9937c2372a1a
Sam Spilsbury <smspillaz@gmail.com>
2015-08-13 20:12:59 +0800

    configure_os: Depend on travis-container >= 0.0.17

--- ciscripts/setup/project/configure_os.py
+++ ciscripts/setup/project/configure_os.py
@@ -268,7 +268,7 @@ def _install_psq_travis_container(cont,
             py_util.pip_install(cont,
                                 util,
                                 "requests",
-                                "polysquare-travis-container>=0.0.15",
+                                "polysquare-travis-container>=0.0.17",
                                 instant_fail=True)
 
     return py_cont
