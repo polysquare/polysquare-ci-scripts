98573ed72bd46eab4c237c6b4d176be5f3770a9c
Sam Spilsbury <smspillaz@gmail.com>
2015-08-26 20:13:33 +0800

    bii: Add coverage hooks
    For bii projects, we need to move the deps folder back into
    place, as we may be relying on it to find some CMakeUnit files

---
+++ ciscripts/coverage/bii/__init__.py
@@ -0,0 +1,6 @@
+# /ciscripts/coverage/bii/__init__.py
+#
+# Module loader file for /ciscripts/coverage/bii.
+#
+# See /LICENCE.md for Copyright information
+"""Module loader file for /ciscripts/coverage/bii."""
