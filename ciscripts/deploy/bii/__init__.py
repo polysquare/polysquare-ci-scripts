009e0395c7cb69ea7345db63097c68724345faba
Sam Spilsbury <smspillaz@gmail.com>
2016-05-09 07:51:14 +0800

    bii: Remove biicode
    Biicode is now defunct and has largely been replaced by
    conan.

--- ciscripts/deploy/bii/__init__.py
+++
@@ -1,6 +0,0 @@
-# /ciscripts/deploy/bii/__init__.py
-#
-# Module loader file for /ciscripts/deploy/bii.
-#
-# See /LICENCE.md for Copyright information
-"""Module loader file for /ciscripts/deploy/bii."""
--- sample/bii/included.cmake
+++
@@ -1,7 +0,0 @@
-# /included.cmake
-#
-# An included file.
-#
-# See /LICENCE.md for Copyright information
-
-set (MY_VARIABLE "ON")
