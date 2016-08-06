6969d69b4aa668f0048df3699be159cb3845e282
Sam Spilsbury <smspillaz@gmail.com>
2016-08-06 21:04:23 +0800

    configure_os: Install polysquare-travis-container 0.0.37

--- ciscripts/setup/project/configure_os.py
+++ ciscripts/setup/project/configure_os.py
@@ -251,7 +251,7 @@ def _install_psq_travis_container(cont,
             py_util.pip_install(cont,
                                 util,
                                 "requests",
-                                "polysquare-travis-container>=0.0.34",
+                                "polysquare-travis-container>=0.0.37",
                                 instant_fail=True)
 
     return py_cont
