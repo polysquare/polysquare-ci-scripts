ae33120a05be1e765d10a61c8d6c39c4d02cd12f
Sam Spilsbury <smspillaz@gmail.com>
2016-08-07 19:58:41 +0800

    configure_os: Bump version of polysquare-travis-container

--- ciscripts/setup/project/configure_os.py
+++ ciscripts/setup/project/configure_os.py
@@ -251,7 +251,7 @@ def _install_psq_travis_container(cont,
             py_util.pip_install(cont,
                                 util,
                                 "requests",
-                                "polysquare-travis-container>=0.0.37",
+                                "polysquare-travis-container>=0.0.38",
                                 instant_fail=True)
 
     return py_cont
