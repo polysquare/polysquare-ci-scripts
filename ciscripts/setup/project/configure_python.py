e9bb8450836d39fd5356f384bdf07b62b91f35e1
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-23 07:10:38 +0000

    Adding tarfile member sanitization to extractall()

--- ciscripts/setup/project/configure_python.py
+++ ciscripts/setup/project/configure_python.py
@@ -284,7 +284,26 @@ def _virtualenv_script(container, util):
                         local_file.write(remote.read())
 
                 with tarfile.open(local_name) as local_tar:
-                    local_tar.extractall()
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(local_tar)
 
                 directory_name = [d for d in os.listdir(".")
                                   if os.path.basename(remote_url) not in d][0]
